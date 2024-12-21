5f8a2feb9253221756bf04d4ab9104ebfc8d3725
guilhermesfranca <guilhermesfranca@outlook.com>
2024-12-21 15:53:36 -0300

    atualizacao dos condicionantes

--- index.js
+++ index.js
@@ -1,20 +1,43 @@
-import input from 'readline-sync'
+import entradaDados from 'readline-sync';
 
-console.log('Aplicação de Juros');
+console.log("Aplicação de Juros: ");
 
-let devido = input.question('Informe o valor devido: ');
-let dias = input.question('informe quantos dias se passaram desde o vencimento: ');
+let valor_juros;
 
-var juros = " "
-if (dias >= 15) {
-    juros = 10
-} else {
-    juros = 5
+let valor_divida = entradaDados.question("\nInforme o valor devido: R$ ");
+
+if(valor_divida > 0)
+{
+    let dias_vencimento = entradaDados.question("Informe quantos dias se passaram desde o vencimento do boleto: ");
+
+    if(dias_vencimento > 0)
+    {
+        if(dias_vencimento > 15)
+        {
+            valor_juros = 10;
+        }
+        else
+        {
+            valor_juros = 5;
+        }
+
+        valor_divida = Number(valor_divida);
+        dias_vencimento = Number(dias_vencimento);
+
+        let total_juros = (valor_divida / 100) * valor_juros;
+        let valor_total = valor_divida + total_juros;
+
+        console.log("\nValor original da dívida: R$ "+valor_divida);
+        console.log("Dias atrasados: "+dias_vencimento);
+        console.log("Taxa de Juros: "+valor_juros+"%");
+        console.log("Valor total com juros: R$ "+valor_total);
+    }
+    else
+    {
+        console.log("Você está em dia!");
+    }
 }
-
-let total = Number(devido) + (Number(devido)*juros)/100
-
-console.log('Valor original da Dívida: R$'+devido)
-console.log('Dias atrasados: '+dias);
-console.log('Taxa de Juros: '+juros+"%");
-console.log('Valor total com juros: R$'+total);
+else
+{
+    console.log("O valor da dívida deve ser maior que zero!")
+}
